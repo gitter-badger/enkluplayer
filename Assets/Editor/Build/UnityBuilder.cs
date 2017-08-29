d3af8d4dee91d61b163c08e50800b67ea46b78e7
Benjamin Jordan <bmj465@gmail.com>
2017-08-29 15:07:25 -0700

    This was ignored by build settings.

---
+++ Assets/Editor/Build/UnityBuilder.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+
+namespace CreateAR.SpirePlayer
+{
+    /// <summary>
+    /// Hooks into Unity build pipeline.
+    /// </summary>
+    public static class UnityBuilder
+    {
+        /// <summary>
+        /// Builds a Unity webgl app.
+        /// </summary>
+        public static void BuildWebGlPlayer()
+        {
+            var options = new BuildPlayerOptions
+            {
+                scenes = new[] { "Assets/Scenes/main.unity" },
+                locationPathName = "../Build",
+                target = BuildTarget.WebGL,
+                options = BuildOptions.AllowDebugging
+                          | BuildOptions.Development
+                          | BuildOptions.ForceEnableAssertions
+            };
+            
+            BuildPipeline.BuildPlayer(options);
+        }
+    }
+}
