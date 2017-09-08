7087bd7109e1bbf5a6cc97df452d78d03cac2ae1
Benjamin Jordan <bmj465@gmail.com>
2017-09-08 13:48:03 -0700

    Stubbing out event router.

---
+++ Assets/Editor/Test/Event/EventRouter_Tests.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using CreateAR.Commons.Unity.Async;
+using NUnit.Framework;
+
+namespace CreateAR.SpirePlayer.Test
+{
+    [TestFixture]
+    public class EventRouter_Tests
+    {
+        public class Message
+        {
+            public string Foo = Guid.NewGuid().ToString();
+        }
+
+        private const int MESSAGE_TYPE_A = 1;
+        private const int MESSAGE_TYPE_B = 2;
+        
+        private MessageRouter _router;
+
+        [SetUp]
+        public void Setup()
+        {
+            _router = new MessageRouter();
+        }
+
+        [Test]
+        public void Subscribe()
+        {
+            var called = false;
+            var message = new Message();
+
+            _router.Subscribe(
+                MESSAGE_TYPE_A,
+                (received, unsub) =>
+                {
+                    called = true;
+
+                    Assert.AreSame(message, received);
+                });
+
+            _router.Publish(
+                MESSAGE_TYPE_A,
+                message);
+
+            Assert.IsTrue(called);
+        }
+
+        [Test]
+        public void MultiSubscribe()
+        {
+            var calledA = false;
+            var calledB = false;
+            var message = new Message();
+
+            _router.Subscribe(
+                MESSAGE_TYPE_A,
+                (received, unsub) =>
+                {
+                    calledA = true;
+
+                    Assert.AreSame(message, received);
+                });
+
+            _router.Subscribe(
+                MESSAGE_TYPE_A,
+                (received, unsub) =>
+                {
+                    calledB = true;
+
+                    Assert.AreSame(message, received);
+                });
+
+            _router.Publish(
+                MESSAGE_TYPE_A,
+                message);
+
+            Assert.IsTrue(calledA);
+            Assert.IsTrue(calledB);
+        }
+
+        [Test]
+        public void SubscribeRelevantEvents()
+        {
+            var called = false;
+            var message = new Message();
+
+            _router.Subscribe(
+                MESSAGE_TYPE_A,
+                (received, unsub) =>
+                {
+                    called = true;
+                });
+
+            _router.Publish(
+                MESSAGE_TYPE_B,
+                message);
+
+            Assert.IsFalse(called);
+        }
+
+        [Test]
+        public void SubscribeMultipleCalls()
+        {
+            var called = 0;
+            var message = new Message();
+
+            _router.Subscribe(
+                MESSAGE_TYPE_A,
+                (received, unsub) =>
+                {
+                    called++;
+                });
+
+            _router.Publish(
+                MESSAGE_TYPE_A,
+                message);
+
+            _router.Publish(
+                MESSAGE_TYPE_A,
+                message);
+
+            Assert.AreEqual(2, called);
+        }
+
+        [Test]
+        public void SubscribeUnsubscribe()
+        {
+            var called = 0;
+            var message = new Message();
+
+            _router.Subscribe(
+                MESSAGE_TYPE_A,
+                (received, unsub) =>
+                {
+                    called++;
+
+                    unsub();
+                });
+
+            _router.Publish(
+                MESSAGE_TYPE_A,
+                message);
+            
+            _router.Publish(
+                MESSAGE_TYPE_A,
+                message);
+
+            Assert.AreEqual(1, called);
+        }
+
+        [Test]
+        public void MultipleSubscribersException()
+        {
+            var called = false;
+            var message = new Message();
+
+            _router.Subscribe(
+                MESSAGE_TYPE_A,
+                (received, unsub) =>
+                {
+                    throw new Exception();
+                });
+
+            _router.Subscribe(
+                MESSAGE_TYPE_A,
+                (received, unsub) =>
+                {
+                    called = true;
+                });
+
+            Assert.Throws<AggregateException>(
+                delegate
+                {
+                    _router.Publish(
+                        MESSAGE_TYPE_A,
+                        message);
+                });
+
+            Assert.IsTrue(called);
+        }
+
+        [Test]
+        public void SubscribeSafety()
+        {
+            var called = false;
+            var message = new Message();
+
+            _router.Subscribe(
+                MESSAGE_TYPE_A,
+                (received, unsub) =>
+                {
+                    // unsub in the middle of a dispatch
+                    unsub();
+                });
+
+            _router.Subscribe(
+                MESSAGE_TYPE_A,
+                (received, unsub) =>
+                {
+                    called = true;
+                });
+
+            _router.Publish(
+                MESSAGE_TYPE_A,
+                message);
+
+            Assert.IsTrue(called);
+        }
+
+        [Test]
+        public void SubscribeCycle()
+        {
+            var called = 0;
+            var message = new Message();
+
+            _router.Subscribe(
+                MESSAGE_TYPE_A,
+                (received, unsub) =>
+                {
+                    called++;
+
+                    // this publish should be discarded
+                    _router.Publish(
+                        MESSAGE_TYPE_A,
+                        message);
+                });
+            
+            _router.Publish(
+                MESSAGE_TYPE_A,
+                message);
+
+            Assert.AreEqual(1, called);
+        }
+    }
+}
---
+++ Assets/Source/Message/MessageRouter.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using CreateAR.Commons.Unity.Async;
+using CreateAR.Commons.Unity.Logging;
+
+namespace CreateAR.SpirePlayer
+{
+    public class MessageRouter
+    {
+        private class SubscriberGroup
+        {
+            public int MessageType { get; private set; }
+
+            public bool IsDispatching = false;
+
+            private readonly List<Action<object>> _subscribers = new List<Action<object>>();
+            private readonly List<Action<object>> _toUnsubscribe = new List<Action<object>>();
+
+            public SubscriberGroup(int messageType)
+            {
+                MessageType = messageType;
+            }
+
+            public void AddSubscriber(Action<object, Action> subscriber)
+            {
+                Action unsub = null;
+                Action<object> action = message => subscriber(message, unsub);
+
+                unsub = () =>
+                {
+                    if (IsDispatching)
+                    {
+                        _toUnsubscribe.Add(action);
+                    }
+                    else
+                    {
+                        _subscribers.Remove(action);
+                    }
+                };
+
+                _subscribers.Add(action);
+            }
+
+            public void Publish(object message)
+            {
+                IsDispatching = true;
+
+                AggregateException aggregate = null;
+
+                for (int i = 0, len = _subscribers.Count; i < len; i++)
+                {
+                    try
+                    {
+                        _subscribers[i](message);
+                    }
+                    catch (Exception exception)
+                    {
+                        if (null == aggregate)
+                        {
+                            aggregate = new AggregateException();
+                        }
+
+                        aggregate.Exceptions.Add(exception);
+                    }
+                }
+
+                // unsubscribes
+                var length = _toUnsubscribe.Count;
+                if (length > 0)
+                {
+                    for (var i = 0; i < length; i++)
+                    {
+                        _subscribers.Remove(_toUnsubscribe[i]);
+                    }
+                    _toUnsubscribe.Clear();
+                }
+
+                IsDispatching = false;
+
+                if (null != aggregate)
+                {
+                    throw aggregate;
+                }
+            }
+        }
+
+        private readonly List<SubscriberGroup> _groups = new List<SubscriberGroup>();
+
+        public void Subscribe(
+            int messageType,
+            Action<object, Action> subscriber)
+        {
+            SubscriberGroup subscribers = null;
+            for (int i = 0, len = _groups.Count; i < len; i++)
+            {
+                var group = _groups[i];
+                if (group.MessageType == messageType)
+                {
+                    subscribers = group;
+
+                    break;
+                }
+            }
+
+            if (null == subscribers)
+            {
+                subscribers = new SubscriberGroup(messageType);
+                _groups.Add(subscribers);
+            }
+
+            subscribers.AddSubscriber(subscriber);
+        }
+
+        public void Publish(
+            int messageType,
+            object message)
+        {
+            for (int i = 0, len = _groups.Count; i < len; i++)
+            {
+                var group = _groups[i];
+                if (group.MessageType == messageType)
+                {
+                    // cannot publish in the middle of a dispatch
+                    if (group.IsDispatching)
+                    {
+                        Log.Warning(this, "Cyclical publish caught in middle of dispatch and discarded.\n\t[{0}] = {1}",
+                            messageType,
+                            message);
+                        return;
+                    }
+
+                    group.Publish(message);
+
+                    break;
+                }
+            }
+        }
+    }
+}
