1fb5bb64fca343f894e467e908cce0593d6ce017
Benjamin Jordan <bmj465@gmail.com>
2017-10-26 12:12:23 -0700

    Removing mp3s for faster import.

---
+++ Assets/Source/Network/Uwp/IUwpWebsocketService.cs
@@ -0,0 +1,24 @@
+﻿namespace CreateAR.SpirePlayer
+{
+    /// <summary>
+    /// Interface for receiving messages from a websocket.
+    /// </summary>
+    public interface IUwpWebsocketService
+    {
+        /// <summary>
+        /// Called when websocket has been opened.
+        /// </summary>
+        void OnOpen();
+
+        /// <summary>
+        /// Called when a message has been received.
+        /// </summary>
+        /// <param name="message">Message that was received.</param>
+        void OnMessage(string message);
+
+        /// <summary>
+        /// Called when the websocket has been closed.
+        /// </summary>
+        void OnClose();
+    }
+}
