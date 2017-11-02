439f57aae5e48e139520eaeca3bc7ace7b889fbb
Benjamin Jordan <bmj465@gmail.com>
2017-11-02 16:32:35 -0700

    Creates children.

--- Assets/Editor/Test/UI/ElementFactory_Tests.cs
+++ Assets/Editor/Test/UI/ElementFactory_Tests.cs
@@ -35,5 +35,115 @@ namespace CreateAR.SpirePlayer.Test
 
             Assert.AreEqual(id, element.Id);
         }
+
+        [Test]
+        public void CreateElementRefChildren()
+        {
+            var a = Guid.NewGuid().ToString();
+            var b = Guid.NewGuid().ToString();
+            var element = _elements.Element(new ElementDescription
+            {
+                Root = new ElementRef
+                {
+                    Id = a,
+                    Children = new []
+                    {
+                        new ElementRef
+                        {
+                            Id = b
+                        }
+                    }
+                },
+                Elements = new[]
+                {
+                    new ElementData
+                    {
+                        Id = a
+                    },
+                    new ElementData
+                    {
+                        Id = b
+                    },
+                }
+            });
+
+            Assert.AreEqual(a, element.Id);
+            Assert.AreEqual(b, element.Children[0].Id);
+        }
+
+        [Test]
+        public void CreateElementManyRefChildren()
+        {
+            var a = Guid.NewGuid().ToString();
+            var b = Guid.NewGuid().ToString();
+            var c = Guid.NewGuid().ToString();
+            var d = Guid.NewGuid().ToString();
+            var e = Guid.NewGuid().ToString();
+
+            var element = _elements.Element(new ElementDescription
+            {
+                Root = new ElementRef
+                {
+                    Id = a,
+                    Children = new[]
+                    {
+                        new ElementRef
+                        {
+                            Id = b
+                        },
+                        new ElementRef
+                        {
+                            Id = c,
+                            Children = new []
+                            {
+                                new ElementRef
+                                {
+                                    Id = d
+                                }
+                            }
+                        },
+                        new ElementRef
+                        {
+                            Id = e
+                        }
+                    }
+                },
+                Elements = new[]
+                {
+                    new ElementData
+                    {
+                        Id = a
+                    },
+                    new ElementData
+                    {
+                        Id = b
+                    },
+                    new ElementData
+                    {
+                        Id = c
+                    },
+                    new ElementData
+                    {
+                        Id = d
+                    },
+                    new ElementData
+                    {
+                        Id = e
+                    }
+                }
+            });
+
+            Assert.AreEqual(a, element.Id);
+            Assert.AreEqual(b, element.Children[0].Id);
+            Assert.AreEqual(c, element.Children[1].Id);
+            Assert.AreEqual(d, element.Children[1].Children[0].Id);
+            Assert.AreEqual(e, element.Children[2].Id);
+        }
+
+        [Test]
+        public void CreateElementChildren()
+        {
+            
+        }
     }
 }
--- Assets/Source/Spire/IUX/Element/Element.cs
+++ Assets/Source/Spire/IUX/Element/Element.cs
@@ -1,4 +1,7 @@
-﻿namespace CreateAR.SpirePlayer.Test
+﻿using System;
+using System.Collections.Generic;
+
+namespace CreateAR.SpirePlayer.Test
 {
     public interface IElementFactory
     {
@@ -18,11 +21,21 @@
             ElementRef reference,
             ElementDescription description)
         {
+            // children first
+            var referencedChildren = reference.Children;
+            var children = new Element[referencedChildren.Length];
+            for (int i = 0, len = referencedChildren.Length; i < len; i++)
+            {
+                children[i] = Element(
+                    referencedChildren[i],
+                    description);
+            }
+
+            // parent
             var id = reference.Id;
             var data = description.ById(id);
-
             var element = new Element();
-            element.Load(data);
+            element.Load(data, children);
 
             return element;
         }
@@ -52,27 +65,67 @@
     public class ElementRef
     {
         public string Id;
-        public ElementRef[] Children;
+        public ElementRef[] Children = new ElementRef[0];
     }
 
     public class ElementData
     {
         public string Id;
-        public ElementData[] Children;
+        public ElementData[] Children = new ElementData[0];
     }
 
     public class Element
     {
+        private readonly List<Element> _elements = new List<Element>();
+
+        public string Guid { get; private set;  }
         public string Id { get; private set; }
 
-        public void Load(ElementData data)
+        public Element[] Children
         {
+            get
+            {
+                return _elements.ToArray();
+            }
+        }
+        
+        public void Load(ElementData data, Element[] children)
+        {
+            Guid = System.Guid.NewGuid().ToString();
             Id = data.Id;
+
+            _elements.AddRange(children);
+
+            LoadInternal();
         }
 
         public void Unload()
         {
+            UnloadInternal();
+
             Id = string.Empty;
+
+            _elements.Clear();
+        }
+
+        public void AddChild(Element child)
+        {
+            _elements.Add(child);
+        }
+
+        public bool RemoveChild(Element child)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected virtual void LoadInternal()
+        {
+            
+        }
+
+        protected virtual void UnloadInternal()
+        {
+
         }
     }
 }
