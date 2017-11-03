0539e6afb8f447787f6b2ce3cbd83d7d65925ca1
Benjamin Jordan <bmj465@gmail.com>
2017-11-03 10:48:32 -0700

    Pulling apart files.

---
+++ Assets/Source/Spire/IUX/Element/ElementSchemaProp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CreateAR.SpirePlayer.UI
+{
+    public class ElementSchemaProp
+    {
+        internal readonly Type Type;
+
+        public ElementSchemaProp(Type type)
+        {
+            Type = type;
+        }
+    }
+
+    public class ElementSchemaProp<T> : ElementSchemaProp
+    {
+        public T Value { get; internal set; }
+
+        public ElementSchemaProp(T value)
+            : base(typeof(T))
+        {
+            Value = value;
+        }
+    }
+}
