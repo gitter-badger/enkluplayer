1b1a27c89b1588b01f3aaa145d98a8901af4f44c
Benjamin Jordan <bmj465@gmail.com>
2017-11-29 08:14:51 -0800

    Added cursor test.

---
+++ Assets/Source/Spire/IUX/CursorTest.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using CreateAR.SpirePlayer.UI;
+
+namespace CreateAR.SpirePlayer
+{
+    public class CursorTest : InjectableMonoBehaviour
+    {
+        [Inject]
+        public IElementFactory Elements { get; set; }
+
+        private void Start()
+        {
+            Elements.Element(new ElementDescription
+            {
+                Elements = new[]
+                {
+                    new ElementData
+                    {
+                        Id = "cursor",
+                        Schema = new ElementSchemaData
+                        {
+                            Ints = new Dictionary<string, int>
+                            {
+                                {"type", ElementTypes.CURSOR}
+                            }
+                        },
+                        Children = new[]
+                        {
+                            new ElementData
+                            {
+                                Id = "reticle",
+                                Schema = new ElementSchemaData
+                                {
+                                    Ints = new Dictionary<string, int>
+                                    {
+                                        {"type", ElementTypes.RETICLE}
+                                    }
+                                },
+                            }
+                        }
+                    },
+                },
+
+                Root = new ElementRef
+                {
+                    Id = "cursor",
+                    Schema = new ElementSchemaData
+                    {
+                        Strings = new Dictionary<string, string>
+                        {
+                            {"name", "Cursor"},
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
