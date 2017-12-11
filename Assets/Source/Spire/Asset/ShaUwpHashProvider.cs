5dd5088789807f27ee81d2b60da21a2eea910e75
Benjamin Jordan <bmj465@gmail.com>
2017-12-11 09:34:22 -0800

    SHA implementation for UWP.

--- Assets/Source/Spire/Asset/ShaUwpHashProvider.cs
+++ Assets/Source/Spire/Asset/ShaUwpHashProvider.cs
@@ -1,11 +1,25 @@
 #if !UNITY_EDITOR && UNITY_WSA
+
+using Windows.Security.Cryptography;
+using Windows.Security.Cryptography.Core;
+
 namespace CreateAR.SpirePlayer.Assets
 {
+    /// <summary>
+    /// UWP implementation.
+    /// </summary>
     public class ShaUwpHashProvider : IHashProvider
     {
+        /// <inheritdoc cref="IHashProvider"/>
         public byte[] Hash(byte[] bytes)
         {
-            return new byte[0];
+            var provider = HashAlgorithmProvider.OpenAlgorithm(HashAlgorithmNames.Sha256);
+            var inBuffer = CryptographicBuffer.CreateFromByteArray(bytes);
+            var outBuffer = provider.HashData(inBuffer);
+
+            CryptographicBuffer.CopyToByteArray(outBuffer, out byte[] outBytes);
+
+            return outBytes;
         }
     }
 }
