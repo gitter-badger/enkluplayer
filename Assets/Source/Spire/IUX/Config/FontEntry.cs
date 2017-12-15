10c91dfa6d406dae16e2ff4e25d2981bf1c53019
Benjamin Jordan <bmj465@gmail.com>
2017-12-15 08:36:33 -0800

    Stubbed out a quick font config.

---
+++ Assets/Source/Spire/IUX/Config/FontEntry.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+namespace CreateAR.SpirePlayer.IUX
+{
+    /// <summary>
+    /// Links a name and a font.
+    /// </summary>
+    [Serializable]
+    public class FontEntry
+    {
+        /// <summary>
+        /// Name associated with this font.
+        /// </summary>
+        public string Name;
+
+        /// <summary>
+        /// The font.
+        /// </summary>
+        public Font Font;
+    }
+}
