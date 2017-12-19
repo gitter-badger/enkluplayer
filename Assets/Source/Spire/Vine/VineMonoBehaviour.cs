8b4be1f2067f626cebfc5f2b576198c1873723f7
Benjamin Jordan <bmj465@gmail.com>
2017-12-19 11:40:53 -0800

    Menu example.

---
+++ Assets/Source/Spire/Vine/VineMonoBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using CreateAR.SpirePlayer.IUX;
+using CreateAR.SpirePlayer.Vine;
+using UnityEngine;
+
+namespace CreateAR.SpirePlayer
+{
+    public class VineMonoBehaviour : InjectableMonoBehaviour
+    {
+        private readonly VineImporter _importer = new VineImporter();
+
+        [Inject]
+        public IElementFactory Elements { get; private set; }
+
+        public TextAsset Vine;
+
+        private void Start()
+        {
+            if (null != Vine)
+            {
+                Elements.Element(_importer.Parse(Vine.text));
+            }
+        }
+    }
+}
