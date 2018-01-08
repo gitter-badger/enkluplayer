83a9b7aebf61964c9673542930112b6a1f75dfb2
Benjamin Jordan <bmj465@gmail.com>
2018-01-08 12:42:36 -0800

    Bwahaha using FieldCache to make handling of disperate types easier.

--- Assets/Editor/Tools/EditorWindow/Toolsuite/WorldScan/WorldScanEditorWindow.cs
+++ Assets/Editor/Tools/EditorWindow/Toolsuite/WorldScan/WorldScanEditorWindow.cs
@@ -12,14 +12,56 @@ namespace CreateAR.SpirePlayer.Editor
     {
         private class TableColumnData
         {
-            public readonly FieldInfo Field;
+            public readonly string Label;
+            public readonly string FieldName;
+            public readonly Type FieldType;
+
             public float Width;
 
-            public TableColumnData(FieldInfo field)
+            public TableColumnData(
+                string label,
+                string fieldName,
+                Type fieldType)
             {
-                Field = field;
+                Label = label;
+                FieldName = fieldName;
+                FieldType = fieldType;
             }
         }
+
+        private class FieldInfoCache
+        {
+            public readonly Type Type;
+            public readonly FieldInfo[] Fields;
+
+            public FieldInfoCache(Type type)
+            {
+                Type = type;
+                Fields = Type.GetFields(BindingFlags.Instance | BindingFlags.Public);
+            }
+
+            public bool Value(
+                string fieldName,
+                Type fieldType,
+                object instance,
+                out object value)
+            {
+                for (int i = 0, len = Fields.Length; i < len; i++)
+                {
+                    var field = Fields[i];
+                    if (field.Name == fieldName && field.FieldType == fieldType)
+                    {
+                        value = field.GetValue(instance);
+                        return true;
+                    }
+                }
+
+                value = null;
+                return false;
+            }
+        }
+
+        private static readonly Dictionary<Type, FieldInfoCache> _caches = new Dictionary<Type, FieldInfoCache>();
 
         private TableColumnData[] _columns;
         private object[] _elements;
@@ -60,7 +102,7 @@ namespace CreateAR.SpirePlayer.Editor
             {
                 foreach (var column in _columns)
                 {
-                    GUILayout.Label(column.Field.Name);
+                    GUILayout.Label(column.Label);
 
                     if (Event.current.type == EventType.Repaint)
                     {
@@ -78,27 +120,36 @@ namespace CreateAR.SpirePlayer.Editor
                 return;
             }
 
-            foreach (var row in _elements)
+            foreach (var element in _elements)
             {
+                var elementType = element.GetType();
+                var cache = _caches[elementType];
+
                 GUILayout.BeginHorizontal();
                 {
                     foreach (var column in _columns)
                     {
-                        object value = null;
-                        try
-                        {
-                            value = column.Field.GetValue(row);
-                        }
-                        catch
-                        {
-                            value = "N/A";
-                        }
+                        object value;
+                        var applicable = cache.Value(
+                            column.FieldName,
+                            column.FieldType,
+                            element,
+                            out value);
 
-                        GUILayout.Label(
-                            null == value
-                                ? (string) GetDefault(column.Field.FieldType)
-                                : value.ToString(),
-                            GUILayout.Width(column.Width));
+                        if (applicable)
+                        {
+                            GUILayout.Label(
+                                null == value
+                                    ? (string) GetDefault(column.FieldType)
+                                    : value.ToString(),
+                                GUILayout.Width(column.Width));
+                        }
+                        else
+                        {
+                            GUILayout.Label(
+                                "N/A",
+                                GUILayout.Width(column.Width));
+                        }
                     }
                 }
                 GUILayout.EndHorizontal();
@@ -117,23 +168,64 @@ namespace CreateAR.SpirePlayer.Editor
                 return;
             }
 
-            var columns = new List<FieldInfo>();
+            var columns = new List<TableColumnData>();
             foreach (var element in _elements)
             {
-                var fields = element.GetType().GetFields(BindingFlags.Instance | BindingFlags.Public);
-                foreach (var field in fields)
+                var type = element.GetType();
+
+                // make sure cache exists for type
+                FieldInfoCache cache;
+                if (!_caches.TryGetValue(type, out cache))
                 {
-                    var exists = columns.Any(column => column.Name == field.Name && column.FieldType == field.FieldType);
-                    if (!exists)
+                    cache = _caches[type] = new FieldInfoCache(type);
+                }
+
+                // add columns
+                foreach (var field in cache.Fields)
+                {
+                    var foundExact = false;
+                    var foundNameOnly = false;
+
+                    foreach (var column in columns)
                     {
-                        columns.Add(field);
+                        if (column.FieldName == field.Name)
+                        {
+                            if (column.FieldType == field.FieldType)
+                            {
+                                foundExact = true;
+
+                                break;
+                            }
+                            else
+                            {
+                                foundNameOnly = true;
+                            }
+                        }
+                    }
+
+                    if (foundExact)
+                    {
+                        continue;
+                    }
+
+                    if (foundNameOnly)
+                    {
+                        columns.Add(new TableColumnData(
+                            string.Format("{0} ({1})", field.Name, field.FieldType.Name),
+                            field.Name,
+                            field.FieldType));
+                    }
+                    else
+                    {
+                        columns.Add(new TableColumnData(
+                            field.Name,
+                            field.Name,
+                            field.FieldType));
                     }
                 }
             }
-            
-            _columns = columns
-                .Select(field => new TableColumnData(field))
-                .ToArray();
+
+            _columns = columns.ToArray();
         }
 
         private object GetDefault(Type type)
