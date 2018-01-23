a0a60e2ac0b6da184638dbbd08954ba163f870f6
Benjamin Jordan <bmj465@gmail.com>
2018-01-23 13:49:16 -0800

    Fixing camera.
    Removing dead scripts from FloatRenderer.

--- Assets/Source/Spire/AR/EditorArService.cs
+++ Assets/Source/Spire/AR/EditorArService.cs
@@ -15,7 +15,7 @@
                     new ArAnchor("floor")
                     {
                         Extents = new Vec3(1, 0, 1),
-                        Position = new Vec3(0.3f, -0.8f, 0.1f),
+                        Position = new Vec3(0f, 0f, 0f),
                         Rotation = Quat.Euler(0, 45, 0)
                     },
                 };
