80b6fd3c0f30b8a5ec0fc6f5b9e0d8aee8a15e75
Benjamin Jordan <bmj465@gmail.com>
2018-02-07 16:44:20 -0800

    Stubbing out prop edit menu (layer on top).

---
+++ Assets/Source/States/Play/Controllers/PropEditController.cs
@@ -0,0 +1,22 @@
+﻿using CreateAR.SpirePlayer.IUX;
+
+namespace CreateAR.SpirePlayer
+{
+    [InjectVine("Prop.Edit")]
+    public class PropEditController : InjectableIUXController
+    {
+        [InjectElements("..btn-move")]
+        public ButtonWidget BtnMove { get; private set; }
+
+        [InjectElements("..btn-delete")]
+        public ButtonWidget BtnDelete { get; private set; }
+
+        [InjectElements("..toggle-fade")]
+        public ToggleWidget ToggleFade { get; private set; }
+
+        public void Initialize(PropData prop)
+        {
+
+        }
+    }
+}
