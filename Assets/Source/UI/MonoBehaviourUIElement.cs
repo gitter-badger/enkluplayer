e72da16f103d37d876aeb76b9e28ad4eff1b9f7f
Benjamin Jordan <bmj465@gmail.com>
2018-05-04 13:37:39 -0700

    Stubbing out basic element factory.

---
+++ Assets/Source/UI/MonoBehaviourUIElement.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace CreateAR.SpirePlayer
+{
+    public class MonoBehaviourUIElement : MonoBehaviour
+    {
+
+    }
+}
---
+++ Assets/Source/UI/SceneUIElementFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CreateAR.Commons.Unity.Async;
+using UnityEngine;
+
+namespace CreateAR.SpirePlayer
+{
+    public class SceneUIElementFactory : MonoBehaviour, IUIElementFactory
+    {
+        [Serializable]
+        public class ElementLink
+        {
+            public string UIDataId;
+            public MonoBehaviourUIElement Element;
+        }
+
+        [Serializable]
+        public class ElementLinkLibrary
+        {
+            public RuntimePlatform[] Platforms;
+
+            public ElementLink[] Links;
+        }
+
+        public ElementLinkLibrary[] Libraries;
+
+        public IAsyncToken<IUIElement> Element(UIReference reference, uint id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
