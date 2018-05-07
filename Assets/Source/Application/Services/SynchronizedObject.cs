ab759baa076599ab0a9d8a95b50360a26dd06fd5
Benjamin Jordan <bmj465@gmail.com>
2018-05-07 15:20:22 -0700

    Comments on SynchronizedObject.

--- Assets/Source/Application/Services/SynchronizedObject.cs
+++ Assets/Source/Application/Services/SynchronizedObject.cs
@@ -3,16 +3,43 @@ using System.Collections.Generic;
 
 namespace CreateAR.SpirePlayer
 {
+    /// <summary>
+    /// Wraps an object and queues asynchronous actions on object. Instead of
+    /// an OnChanged event, the subscriber to changes is also asyncronous.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class SynchronizedObject<T>
     {
-        private readonly Queue<Action<T, Action<T>>> _mods = new Queue<Action<T, Action<T>>>();
+        /// <summary>
+        /// Actions to perform.
+        /// </summary>
+        private readonly Queue<Action<T, Action<T>>> _actions = new Queue<Action<T, Action<T>>>();
+
+        /// <summary>
+        /// The subscriber.
+        /// </summary>
         private readonly Action<T, Action> _subscriber;
 
-        private bool _isProcessing = false;
-        private bool _isWaitingOnSubscriber = false;
+        /// <summary>
+        /// True iff the an action is processing.
+        /// </summary>
+        private bool _isWaitingOnProcessing;
 
+        /// <summary>
+        /// True iff the subscriber is processing.
+        /// </summary>
+        private bool _isWaitingOnSubscriber;
+
+        /// <summary>
+        /// The underlying data.
+        /// </summary>
         public T Data { get; private set; }
         
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="data">The initial data.</param>
+        /// <param name="subscriber">The subscriber. Called every time the data is changed.</param>
         public SynchronizedObject(T data, Action<T, Action> subscriber)
         {
             Data = data;
@@ -20,9 +47,13 @@ namespace CreateAR.SpirePlayer
             _subscriber = subscriber;
         }
 
-        public void Queue(Action<T, Action<T>> mod)
+        /// <summary>
+        /// Queues an asynchronous action on an object.
+        /// </summary>
+        /// <param name="action">The action to take.</param>
+        public void Queue(Action<T, Action<T>> action)
         {
-            _mods.Enqueue(mod);
+            _actions.Enqueue(action);
 
             if (!_isWaitingOnSubscriber)
             {
@@ -30,29 +61,36 @@ namespace CreateAR.SpirePlayer
             }
         }
 
+        /// <summary>
+        /// Processes the next item in the queue.
+        /// </summary>
         private void ProcessQueue()
         {
-            if (_isProcessing)
+            if (_isWaitingOnProcessing)
             {
                 return;
             }
 
-            if (_mods.Count == 0)
+            if (_actions.Count == 0)
             {
                 return;
             }
 
-            _isProcessing = true;
+            _isWaitingOnProcessing = true;
 
-            var action = _mods.Dequeue();
+            var action = _actions.Dequeue();
             action(Data, OnComplete);
         }
 
+        /// <summary>
+        /// Called when the action is complete.
+        /// </summary>
+        /// <param name="val">The new value.</param>
         private void OnComplete(T val)
         {
             Data = val;
 
-            _isProcessing = false;
+            _isWaitingOnProcessing = false;
             _isWaitingOnSubscriber = true;
             _subscriber(Data, () =>
             {
