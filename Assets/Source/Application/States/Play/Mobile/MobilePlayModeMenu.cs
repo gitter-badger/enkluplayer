27b2433e216441f3f42e6d9c6b91d2300c031a3b
Benjamin Jordan <bmj465@gmail.com>
2018-05-10 12:36:31 -0700

    W00t, basic play menu.

---
+++ Assets/Source/Application/States/Play/Mobile/MobilePlayModeMenu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CreateAR.SpirePlayer
+{
+    /// <summary>
+    /// Play mode menu for mobile.
+    /// </summary>
+    public class MobilePlayModeMenu : MonoBehaviourUIElement
+    {
+        /// <summary>
+        /// Called when insta button has been clicked.
+        /// </summary>
+        public event Action OnInstaClicked;
+
+        /// <summary>
+        /// Called when back button has been clicked.
+        /// </summary>
+        public event Action OnBackClicked;
+
+        /// <summary>
+        /// Called by Unity when back is clicked.
+        /// </summary>
+        public void BackClicked()
+        {
+            if (null != OnBackClicked)
+            {
+                OnBackClicked();
+            }
+        }
+        
+        /// <summary>
+        /// Called by Unity when insta is clicked.
+        /// </summary>
+        public void InstaClicked()
+        {
+            if (null != OnInstaClicked)
+            {
+                OnInstaClicked();
+            }
+        }
+    }
+}
