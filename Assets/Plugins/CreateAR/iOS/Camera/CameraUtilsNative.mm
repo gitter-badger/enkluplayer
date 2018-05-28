b39e60ff5b815146aa4a18c1ab71f38618646b80
Benjamin Jordan <bmj465@gmail.com>
2018-05-28 11:05:13 -0700

    Fixing native lib, bumping minor.

--- Assets/Plugins/CreateAR/iOS/Camera/CameraUtilsNative.mm
+++ Assets/Plugins/CreateAR/iOS/Camera/CameraUtilsNative.mm
@@ -1,23 +1,24 @@
 #include "stdlib.h"
 #include <AVFoundation/AVFoundation.h>
 #import <UIKit/UIKit.h>
+#import <Photos/Photos.h>
 
 extern "C"
 {
     bool unity_hasCameraPermissions()
     {
         AVAuthorizationStatus authStatus = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
-
+        
         return authStatus == AVAuthorizationStatusAuthorized;
     }
-
+    
     bool unity_hasDeniedCameraPermissions()
     {
         AVAuthorizationStatus authStatus = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
-
+        
         return authStatus == AVAuthorizationStatusDenied;
     }
-
+    
     void unity_requestCameraPermissions()
     {
         [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
@@ -31,22 +32,22 @@ extern "C"
             }
         }];
     }
-
+    
     void unity_openSettings()
     {
         [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]];
     }
     
-    void unity_hasPhotosPermissions()
+    bool unity_hasPhotosPermissions()
     {
         PHAuthorizationStatus status = [PHPhotoLibrary authorizationStatus];
-        return authStatus == PHAuthorizationStatusAuthorized;
+        return status == PHAuthorizationStatusAuthorized;
     }
     
-    void unity_hasDeniedPhotosPermissions()
+    bool unity_hasDeniedPhotosPermissions()
     {
         PHAuthorizationStatus status = [PHPhotoLibrary authorizationStatus];
-        return authStatus == PHAuthorizationStatusDenied;
+        return status == PHAuthorizationStatusDenied;
     }
     
     void unity_requestPhotosPermissions()
@@ -54,12 +55,12 @@ extern "C"
         [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
             if (status == PHAuthorizationStatusAuthorized)
             {
-                NSLog("@Photo library access granted.");
+                NSLog(@"Photo library access granted.");
             }
             else
             {
                 NSLog(@"Photo library access denied.");
             }
-        }]
+        }];
     }
 }
