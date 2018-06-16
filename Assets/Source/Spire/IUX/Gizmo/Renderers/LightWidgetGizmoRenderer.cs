698ed9d01c107c09d4db3fb01a0ac279079aa844
Benjamin Jordan <bmj465@gmail.com>
2018-06-16 16:35:07 -0500

    Respect spot range + angle.

--- Assets/Source/Spire/IUX/Gizmo/Renderers/LightWidgetGizmoRenderer.cs
+++ Assets/Source/Spire/IUX/Gizmo/Renderers/LightWidgetGizmoRenderer.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using CreateAR.Commons.Unity.Logging;
 using RTEditor;
 using UnityEngine;
 
@@ -14,6 +13,9 @@ namespace CreateAR.SpirePlayer.IUX
         /// Props.
         /// </summary>
         private ElementSchemaProp<string> _typeProp;
+        private ElementSchemaProp<float> _pointRangeProp;
+        private ElementSchemaProp<float> _spotRangeProp;
+        private ElementSchemaProp<float> _angleProp;
 
         /// <summary>
         /// Cached camera.
@@ -34,7 +36,10 @@ namespace CreateAR.SpirePlayer.IUX
             schema.Set("visible", true);
 
             _typeProp = schema.Get<string>("lightType");
-            
+            _pointRangeProp = schema.Get<float>("point.range");
+            _spotRangeProp = schema.Get<float>("spot.range");
+            _angleProp = schema.Get<float>("spot.angle");
+
             TextureRenderer.enabled = true;
         }
 
@@ -62,82 +67,184 @@ namespace CreateAR.SpirePlayer.IUX
                 _cam = editorCam.GetComponent<Camera>();
             }
             
-            // grab transform values here
-            var screen = _cam.WorldToScreenPoint(transform.position);
-            var forward = _cam.WorldToScreenPoint(transform.position + transform.forward);
-            var right = _cam.WorldToScreenPoint(transform.position + transform.right);
-            var up = _cam.WorldToScreenPoint(transform.position + transform.up);
-
             var type = ToLightType(_typeProp.Value);
             switch (type)
             {
                 case LightType.Directional:
                 {
-                    var handle = Render.Handle2D("Gizmo");
-                    if (null != handle)
-                    {
-                        handle.Draw(ctx =>
-                        {
-                            var screenForward = (forward - screen).normalized;
-                            var screenRight = (right - screen).normalized;
-                            var screenUp = (up - screen).normalized;
-                            var screenLength = 100f;
-                            
-                            // main forward
-                            ctx.Color(Color.yellow);
-                            ctx.Line(screen, screen + screenLength * screenForward);
-
-                            // box
-                            const float screenBoxDistance = 50f;
-                            const float screenBoxHalfDistance = screenBoxDistance / 2f;
-
-                            ctx.Line(
-                                screen + screenBoxHalfDistance * screenUp + screenBoxHalfDistance * screenRight,
-                                screen + screenBoxHalfDistance * screenUp - screenBoxHalfDistance * screenRight);
-                            ctx.Line(
-                                screen + screenBoxHalfDistance * screenUp - screenBoxHalfDistance * screenRight,
-                                screen - screenBoxHalfDistance * screenUp - screenBoxHalfDistance * screenRight);
-                            ctx.Line(
-                                screen - screenBoxHalfDistance * screenUp - screenBoxHalfDistance * screenRight,
-                                screen - screenBoxHalfDistance * screenUp + screenBoxHalfDistance * screenRight);
-                            ctx.Line(
-                                screen - screenBoxHalfDistance * screenUp + screenBoxHalfDistance * screenRight,
-                                screen + screenBoxHalfDistance * screenUp + screenBoxHalfDistance * screenRight);
-
-                            // shoots
-                            const float screenShootDistance = 40f;
-                            const float screenShootHalfDistance = screenShootDistance / 2f;
-                            const float screenShootLength = 75f;
-
-                            ctx.Line(
-                                screen + screenShootHalfDistance * screenUp + screenShootHalfDistance * screenRight,
-                                screen + screenShootHalfDistance * screenUp + screenShootHalfDistance * screenRight + screenShootLength * screenForward);
-                            ctx.Line(
-                                screen + screenShootHalfDistance * screenUp - screenShootHalfDistance * screenRight,
-                                screen + screenShootHalfDistance * screenUp - screenShootHalfDistance * screenRight + screenShootLength * screenForward);
-                            ctx.Line(
-                                screen - screenShootHalfDistance * screenUp - screenShootHalfDistance * screenRight,
-                                screen - screenShootHalfDistance * screenUp - screenShootHalfDistance * screenRight + screenShootLength * screenForward);
-                            ctx.Line(
-                                screen - screenShootHalfDistance * screenUp + screenShootHalfDistance * screenRight,
-                                screen - screenShootHalfDistance * screenUp + screenShootHalfDistance * screenRight + screenShootLength * screenForward);
-                        });
-                    }
+                    DrawDirectionalGizmo();
                     break;
                 }
                 case LightType.Point:
                 {
-                    Log.Info(this, "Point");
-                        break;
+                    DrawPointGizmo();
+                    break;
                 }
                 case LightType.Spot:
                 {
-                    Log.Info(this, "Spot");
+                    DrawSpotGizmo();
                     break;
                 }
             }
         }
-        
+
+        /// <summary>
+        /// Draws gizmos for directional lights.
+        /// </summary>
+        private void DrawDirectionalGizmo()
+        {
+            var handle = Render.Handle2D("Gizmo");
+            if (null != handle)
+            {
+                // grab transform values here
+                var screen = _cam.WorldToScreenPoint(transform.position);
+                var forward = _cam.WorldToScreenPoint(transform.position + transform.forward);
+                var right = _cam.WorldToScreenPoint(transform.position + transform.right);
+                var up = _cam.WorldToScreenPoint(transform.position + transform.up);
+                handle.Draw(ctx =>
+                {
+                    var screenForward = (forward - screen).normalized;
+                    var screenRight = (right - screen).normalized;
+                    var screenUp = (up - screen).normalized;
+                    var screenLength = 100f;
+
+                    // main forward
+                    ctx.Color(Color.yellow);
+                    ctx.Line(screen, screen + screenLength * screenForward);
+
+                    // box
+                    const float screenBoxDistance = 50f;
+                    const float screenBoxHalfDistance = screenBoxDistance / 2f;
+
+                    ctx.Line(
+                        screen + screenBoxHalfDistance * screenUp + screenBoxHalfDistance * screenRight,
+                        screen + screenBoxHalfDistance * screenUp - screenBoxHalfDistance * screenRight);
+                    ctx.Line(
+                        screen + screenBoxHalfDistance * screenUp - screenBoxHalfDistance * screenRight,
+                        screen - screenBoxHalfDistance * screenUp - screenBoxHalfDistance * screenRight);
+                    ctx.Line(
+                        screen - screenBoxHalfDistance * screenUp - screenBoxHalfDistance * screenRight,
+                        screen - screenBoxHalfDistance * screenUp + screenBoxHalfDistance * screenRight);
+                    ctx.Line(
+                        screen - screenBoxHalfDistance * screenUp + screenBoxHalfDistance * screenRight,
+                        screen + screenBoxHalfDistance * screenUp + screenBoxHalfDistance * screenRight);
+
+                    // shoots
+                    const float screenShootDistance = 40f;
+                    const float screenShootHalfDistance = screenShootDistance / 2f;
+                    const float screenShootLength = 75f;
+
+                    ctx.Line(
+                        screen + screenShootHalfDistance * screenUp + screenShootHalfDistance * screenRight,
+                        screen + screenShootHalfDistance * screenUp + screenShootHalfDistance * screenRight +
+                        screenShootLength * screenForward);
+                    ctx.Line(
+                        screen + screenShootHalfDistance * screenUp - screenShootHalfDistance * screenRight,
+                        screen + screenShootHalfDistance * screenUp - screenShootHalfDistance * screenRight +
+                        screenShootLength * screenForward);
+                    ctx.Line(
+                        screen - screenShootHalfDistance * screenUp - screenShootHalfDistance * screenRight,
+                        screen - screenShootHalfDistance * screenUp - screenShootHalfDistance * screenRight +
+                        screenShootLength * screenForward);
+                    ctx.Line(
+                        screen - screenShootHalfDistance * screenUp + screenShootHalfDistance * screenRight,
+                        screen - screenShootHalfDistance * screenUp + screenShootHalfDistance * screenRight +
+                        screenShootLength * screenForward);
+                });
+            }
+        }
+
+        /// <summary>
+        /// Draws gizmos for point lights.
+        /// </summary>
+        private void DrawPointGizmo()
+        {
+            var handle = Render.Handle2D("Gizmo");
+            if (null != handle)
+            {
+                // grab transform values here
+                var len = _pointRangeProp.Value;
+                var pos = transform.position;
+                var extents = new[]
+                {
+                    _cam.WorldToScreenPoint(pos - len * transform.forward),
+                    _cam.WorldToScreenPoint(pos + len * transform.forward),
+
+                    _cam.WorldToScreenPoint(pos - len * transform.up),
+                    _cam.WorldToScreenPoint(pos + len * transform.up),
+
+                    _cam.WorldToScreenPoint(pos - len * transform.right),
+                    _cam.WorldToScreenPoint(pos + len * transform.right),
+
+                    _cam.WorldToScreenPoint(pos - (len * transform.right + len * transform.up)),
+                    _cam.WorldToScreenPoint(pos + (len * transform.right + len * transform.up)),
+
+                    _cam.WorldToScreenPoint(pos - (len * transform.right + len * transform.forward)),
+                    _cam.WorldToScreenPoint(pos + (len * transform.right + len * transform.forward)),
+
+                    _cam.WorldToScreenPoint(pos - (len * transform.forward + len * transform.up)),
+                    _cam.WorldToScreenPoint(pos + (len * transform.forward + len * transform.up))
+                };
+                
+                handle.Draw(ctx =>
+                {
+                    // each extent
+                    ctx.Color(Color.yellow);
+
+                    ctx.Line(extents[0], extents[1]);
+                    ctx.Line(extents[2], extents[3]);
+                    ctx.Line(extents[4], extents[5]);
+                    ctx.Line(extents[6], extents[7]);
+                    ctx.Line(extents[8], extents[9]);
+                    ctx.Line(extents[10], extents[11]);
+                });
+            }
+        }
+
+        /// <summary>
+        /// Draws gizmos for spot lights.
+        /// </summary>
+        private void DrawSpotGizmo()
+        {
+            var handle = Render.Handle2D("Gizmo");
+            if (null != handle)
+            {
+                // grab transform values here
+                var pos = transform.position;
+                var forward = transform.forward;
+                var right = transform.right;
+                var up = transform.up;
+
+                var screen = _cam.WorldToScreenPoint(pos);
+
+                var range = _spotRangeProp.Value;
+                var angle = 90f - _angleProp.Value / 2f;
+                var len = range / Mathf.Sin(Mathf.Deg2Rad * angle);
+                
+                var extents = new[]
+                {
+                    _cam.WorldToScreenPoint(pos + range * forward + len * right),
+                    _cam.WorldToScreenPoint(pos + range * forward - len * right),
+                    _cam.WorldToScreenPoint(pos + range * forward + len * up),
+                    _cam.WorldToScreenPoint(pos + range * forward - len * up)
+                };
+                
+                handle.Draw(ctx =>
+                {
+                    ctx.Color(Color.yellow);
+                    ctx.Line(screen, extents[0]);
+                    ctx.Line(screen, extents[1]);
+                    ctx.Line(screen, extents[2]);
+                    ctx.Line(screen, extents[3]);
+
+                    ctx.Line(extents[0], extents[2]);
+                    ctx.Line(extents[1], extents[2]);
+                    ctx.Line(extents[3], extents[0]);
+                    ctx.Line(extents[3], extents[1]);
+                });
+            }
+        }
+
         /// <summary>
         /// Parses light type.
         /// </summary>
