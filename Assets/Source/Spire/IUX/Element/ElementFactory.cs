117be06b9720524ce95964f5d11b4d77cff43221
Benjamin Jordan <bmj465@gmail.com>
2018-07-25 14:11:18 -0700

    Lower default page size.

--- Assets/Source/Spire/IUX/Element/ElementFactory.cs
+++ Assets/Source/Spire/IUX/Element/ElementFactory.cs
@@ -170,7 +170,7 @@ namespace CreateAR.SpirePlayer.IUX
                 {
                     { "fontSize", 80 },
                     { "header.width", 700 },
-                    { "page.size", 5 },
+                    { "page.size", 4 },
                 }
             });
             menuSchema.Inherit(_baseSchema);
