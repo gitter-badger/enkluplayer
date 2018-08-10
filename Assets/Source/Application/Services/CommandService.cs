19a64b347bc34069cea3b04663ce54ad9c08128f
Benjamin Jordan <bmj465@gmail.com>
2018-08-10 11:05:39 -0700

    Built out basic command service.

---
+++ Assets/Source/Application/Services/CommandService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using CreateAR.Commons.Unity.Messaging;
+
+namespace CreateAR.SpirePlayer
+{
+    /// <summary>
+    /// Base implementation of service that receives commands.
+    /// </summary>
+    public class CommandService : ApplicationService
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public CommandService(
+            MessageTypeBinder binder,
+            IMessageRouter messages)
+            : base(binder, messages)
+        {
+            //
+        }
+
+        /// <summary>
+        /// Sets a handler by type.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <param name="handler">The associated handler.</param>
+        public virtual void SetHandler(string type, Action<string, ICommandClient> handler)
+        {
+            //
+        }
+
+        /// <summary>
+        /// Removes a handler.
+        /// </summary>
+        /// <param name="type">The type the handler was associated with.</param>
+        public virtual void RemoveHandler(string type)
+        {
+            //
+        }
+    }
+}
--- Assets/Source/Application/States/Initialize/InitializeApplicationState.cs
+++ Assets/Source/Application/States/Initialize/InitializeApplicationState.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using CreateAR.Commons.Unity.Async;
+using CreateAR.Commons.Unity.Logging;
 using CreateAR.Commons.Unity.Messaging;
 using CreateAR.SpirePlayer.Assets;
 using CreateAR.SpirePlayer.BLE;
@@ -22,7 +25,9 @@ namespace CreateAR.SpirePlayer
         private readonly IBleService _ble;
         private readonly IWorldAnchorProvider _anchors;
         private readonly IAppSceneManager _scenes;
+        private readonly CommandService _commands;
         private readonly BleServiceConfiguration _bleConfig;
+
         /// <summary>
         /// Constructor.
         /// </summary>
@@ -33,15 +38,17 @@ namespace CreateAR.SpirePlayer
             IBleService ble,
             IWorldAnchorProvider anchors,
             IAppSceneManager scenes,
+            CommandService commands,
             BleServiceConfiguration bleConfig)
         {
             _messages = messages;
             _assets = assets;
-            _assetLoader = assetLoader;
-            _bleConfig = bleConfig;
+            _assetLoader = assetLoader;            
             _ble = ble;
             _anchors = anchors;
             _scenes = scenes;
+            _commands = commands;
+            _bleConfig = bleConfig;
         }
 
         /// <inheritdoc cref="IState"/>
@@ -52,6 +59,8 @@ namespace CreateAR.SpirePlayer
             
             // reset assets
             _assets.Uninitialize();
+
+            AddCommands();
             
             // wait for tasks to finish
             var tasks = new List<IAsyncToken<Void>>
@@ -90,6 +99,57 @@ namespace CreateAR.SpirePlayer
         public void Exit()
         {
             
+        }
+
+        /// <summary>
+        /// Adds all commands to command service.
+        /// </summary>
+        private void AddCommands()
+        {
+            _commands.SetHandler("log", Commands_OnLog);
+        }
+
+        /// <summary>
+        /// Handles logging commands.
+        /// </summary>
+        /// <param name="command">The command.</param>
+        /// <param name="client">The client.</param>
+        private void Commands_OnLog(string command, ICommandClient client)
+        {
+            // log -l [Debug|...|Fatal] (Optional) --off (Optional)
+            //
+            // -l sets the log level for all clients.
+            // --off turns off logging to specific client.
+
+            // prep the log target first
+            var target = Log.Targets.OfType<CommandClientLogTarget>().FirstOrDefault();
+            if (null == target)
+            {
+                target = new CommandClientLogTarget(new DefaultLogFormatter
+                {
+                    Level = true,
+                    ObjectToString = true,
+                    Timestamp = true,
+                    TypeName = true
+                });
+
+                Log.AddLogTarget(target);
+
+                target.Add(client);
+            }
+
+            // configure level
+            string level;
+            if (CommandParser.Value('l', command, out level))
+            {
+                target.Filter = (LogLevel) Enum.Parse(typeof(LogLevel), level);
+            }
+
+            // handle off
+            if (CommandParser.Toggle("off", command))
+            {
+                target.Remove(client);
+            }
         }
     }
 }
