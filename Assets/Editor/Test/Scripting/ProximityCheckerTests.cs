a334b4be9875f2fd89b40df0c371f6b8b6064acf
James Sundra <pojo226@gmail.com>
2018-08-23 16:33:56 -0700

    Added ProximityChecker & matching tests.

---
+++ Assets/Editor/Test/Scripting/ProximityCheckerTests.cs
@@ -0,0 +1,385 @@
+﻿using CreateAR.SpirePlayer.IUX;
+using CreateAR.SpirePlayer.Scripting;
+using Jint;
+using NUnit.Framework;
+
+namespace CreateAR.SpirePlayer.Test.Scripting
+{
+    /// <summary>
+    /// Tests the actual <see cref="ProximityChecker"/> class, with a wide range of mock scene configurations
+    /// </summary>
+    [TestFixture]
+    public class ProximityChecker_Tests
+    {
+        private Engine _engine;
+        private ProximityChecker _proximityChecker;
+
+        private ElementJs _elementA;
+        private ElementJs _elementB;
+
+        private int enterCount = 0;
+        private int stayCount = 0;
+        private int exitCount = 0;
+
+        [SetUp]
+        public void Setup()
+        {
+            _engine = new Engine();
+            _proximityChecker = new ProximityChecker();
+
+            enterCount = 0;
+            stayCount = 0;
+            exitCount = 0;
+
+            _proximityChecker.OnEnter += (ElementJs callbackListener, ElementJs callbackTrigger) => {
+                Assert.AreEqual(_elementA, callbackListener);
+                Assert.AreEqual(_elementB, callbackTrigger);
+                enterCount++;
+            };
+
+            _proximityChecker.OnStay += (ElementJs callbackListener, ElementJs callbackTrigger) => {
+                Assert.AreEqual(_elementA, callbackListener);
+                Assert.AreEqual(_elementB, callbackTrigger);
+                stayCount++;
+            };
+
+            _proximityChecker.OnExit += (ElementJs callbackListener, ElementJs callbackTrigger) => {
+                Assert.AreEqual(_elementA, callbackListener);
+                Assert.AreEqual(_elementB, callbackTrigger);
+                exitCount++;
+            };
+        }
+
+        [Test]
+        public void SimpleEventFiring()
+        {
+            // Setup elements. A is a listener, B is a trigger
+            _elementA = BuildElementJs(true, false, 3, 5);
+            _elementB = BuildElementJs(false, true, 2, 5);
+
+            // Check for no-op
+            _elementB.transform.position = new Vec3(10, 0, 0);
+            _proximityChecker.Update();
+            CheckCallbackCounts(0, 0, 0);
+
+            // Check for enter
+            _elementB.transform.position = new Vec3(4, 0, 0);
+            _proximityChecker.Update();
+            CheckCallbackCounts(1, 0, 0);
+
+            // Check for stay (between inner/outer)
+            _elementB.transform.position = new Vec3(7, 0, 0);
+            _proximityChecker.Update();
+            CheckCallbackCounts(1, 1, 0);
+
+            // Check for exit
+            _elementB.transform.position = new Vec3(12, 0, 0);
+            _proximityChecker.Update();
+            CheckCallbackCounts(1, 1, 1);
+        }
+
+        [Test]
+        public void ListenersIgnoreEachOther()
+        {
+            // Setup elements. A and B are both listeners
+            ElementJs listenerA = BuildElementJs(true, false, 2.5f, 5);
+            ElementJs listenerB = BuildElementJs(true, false, 2.5f, 5);
+
+            // Check for no-op
+            listenerB.transform.position = new Vec3(10, 0, 0);
+            _proximityChecker.Update();
+            CheckCallbackCounts(0, 0, 0);
+
+            // Check for no-op again
+            listenerB.transform.position = new Vec3(4, 0, 0);
+            _proximityChecker.Update();
+            CheckCallbackCounts(0, 0, 0);
+        }
+
+        [Test]
+        public void TriggersIgnoreEachOther()
+        {
+            // Setup elements. A and B are both triggers
+            ElementJs triggerA = BuildElementJs(false, true, 2.5f, 5);
+            ElementJs triggerB = BuildElementJs(false, true, 2.5f, 5);
+
+            // Check for no-op
+            triggerB.transform.position = new Vec3(10, 0, 0);
+            _proximityChecker.Update();
+            CheckCallbackCounts(0, 0, 0);
+
+            // Check for no-op again
+            triggerB.transform.position = new Vec3(4, 0, 0);
+            _proximityChecker.Update();
+            CheckCallbackCounts(0, 0, 0);
+        }
+
+        [Test]
+        public void ChildrenAreIgnored()
+        {
+            // Setup elements. A is a listener, B is a trigger
+            ElementJs parent = BuildElementJs(true, false, 2.5f, 5);
+            ElementJs child = BuildElementJs(false, true, 2.5f, 5);
+            parent.addChild(child);
+
+
+            // Check for no-op
+            child.transform.position = new Vec3(10, 0, 0);
+            _proximityChecker.Update();
+            CheckCallbackCounts(0, 0, 0);
+
+            // Check for no-op again
+            child.transform.position = new Vec3(4, 0, 0);
+            _proximityChecker.Update();
+            CheckCallbackCounts(0, 0, 0);
+        }
+
+        [Test]
+        public void ParentsAreIgnored()
+        {
+            // Setup elements. A is a listener, B is a trigger
+            ElementJs parent = BuildElementJs(false, true, 2.5f, 5);
+            ElementJs child = BuildElementJs(true, false, 2.5f, 5);
+            parent.addChild(child);
+
+            // Check for no-op
+            child.transform.position = new Vec3(10, 0, 0);
+            _proximityChecker.Update();
+            CheckCallbackCounts(0, 0, 0);
+
+            // Check for no-op again
+            child.transform.position = new Vec3(4, 0, 0);
+            _proximityChecker.Update();
+            CheckCallbackCounts(0, 0, 0);
+        }
+
+        /// <summary>
+        /// Check that a listening Element can also be a trigger. Callback counts in this test will be 2x to check for bi-directionality.
+        /// </summary>
+        [Test]
+        public void ListenersCanBeTriggers()
+        {
+            // Both elements are listeners and triggers
+            ElementJs mixedA = BuildElementJs(true, true, 3, 5);
+            ElementJs mixedB = BuildElementJs(true, true, 2, 5);
+
+            // We'll need some custom callback logic, so clear the existing ones
+            _proximityChecker.OnEnter = null;
+            _proximityChecker.OnStay = null;
+            _proximityChecker.OnExit = null;
+
+            ElementJs cachedListenerElement = null;
+            ElementJs cachedTriggerElement = null;
+
+            _proximityChecker.OnEnter += (ElementJs callbackListener, ElementJs callbackTrigger) => {
+                Assert.AreNotEqual(callbackListener, callbackTrigger);
+
+                // Gross, but we need to check that the callbacks are actually bi-directional and not just 2x
+                if (cachedListenerElement != null)
+                {
+                    Assert.AreNotEqual(cachedListenerElement, callbackListener);
+                }
+                cachedListenerElement = callbackListener;
+
+                if (cachedTriggerElement != null)
+                {
+                    Assert.AreNotEqual(cachedTriggerElement, callbackTrigger);
+                    
+                }
+                cachedTriggerElement = callbackTrigger;
+
+                enterCount++;
+            };
+
+            _proximityChecker.OnStay += (ElementJs callbackListener, ElementJs callbackTrigger) => {
+                Assert.AreNotEqual(callbackListener, callbackTrigger);
+
+                // Gross, but we need to check that the callbacks are actually bi-directional and not just 2x
+                if (cachedListenerElement != null)
+                {
+                    Assert.AreNotEqual(cachedListenerElement, callbackListener);
+                }
+                cachedListenerElement = callbackListener;
+
+                if (cachedTriggerElement != null)
+                {
+                    Assert.AreNotEqual(cachedTriggerElement, callbackTrigger);
+
+                }
+                cachedTriggerElement = callbackTrigger;
+
+                stayCount++;
+            };
+
+            _proximityChecker.OnExit += (ElementJs callbackListener, ElementJs callbackTrigger) => {
+                Assert.AreNotEqual(callbackListener, callbackTrigger);
+
+                // Gross, but we need to check that the callbacks are actually bi-directional and not just 2x
+                if (cachedListenerElement != null)
+                {
+                    Assert.AreNotEqual(cachedListenerElement, callbackListener);
+                }
+                cachedListenerElement = callbackListener;
+
+                if (cachedTriggerElement != null)
+                {
+                    Assert.AreNotEqual(cachedTriggerElement, callbackTrigger);
+
+                }
+                cachedTriggerElement = callbackTrigger;
+
+                exitCount++;
+            };
+
+
+            // Check for no-op
+            mixedB.transform.position = new Vec3(10, 0, 0);
+            _proximityChecker.Update();
+            CheckCallbackCounts(0, 0, 0);
+
+            // Check for enter
+            mixedB.transform.position = new Vec3(4, 0, 0);
+            _proximityChecker.Update();
+            CheckCallbackCounts(2, 0, 0);
+            Assert.NotNull(cachedListenerElement);
+            Assert.NotNull(cachedTriggerElement);
+            cachedListenerElement = null;
+            cachedTriggerElement = null;
+
+            // Check for stay (between inner/outer)
+            mixedB.transform.position = new Vec3(7, 0, 0);
+            _proximityChecker.Update();
+            CheckCallbackCounts(2, 2, 0);
+            Assert.NotNull(cachedListenerElement);
+            Assert.NotNull(cachedTriggerElement);
+            cachedListenerElement = null;
+            cachedTriggerElement = null;
+
+            // Check for exit
+            mixedB.transform.position = new Vec3(12, 0, 0);
+            _proximityChecker.Update();
+            CheckCallbackCounts(2, 2, 2);
+            Assert.NotNull(cachedListenerElement);
+            Assert.NotNull(cachedTriggerElement);
+            cachedListenerElement = null;
+            cachedTriggerElement = null;
+        }
+
+        [Test]
+        public void MultipleCollisions()
+        {
+            // Create 1 element as a listener, and 2 triggers
+            ElementJs listener = BuildElementJs(true, false, 5, 10);
+            _elementA = BuildElementJs(false, true, 2, 5);
+            _elementB = BuildElementJs(false, true, 2, 5);
+
+            // We'll need some custom callback logic, so clear the existing ones
+            _proximityChecker.OnEnter = null;
+            _proximityChecker.OnStay = null;
+            _proximityChecker.OnExit = null;
+            
+            ElementJs cachedEnterTrigger = null;
+            ElementJs cachedStayTrigger = null;
+            ElementJs cachedExitTrigger = null;
+
+            _proximityChecker.OnEnter += (ElementJs callbackListener, ElementJs callbackTrigger) => {
+                Assert.AreEqual(listener, callbackListener);
+                Assert.AreNotEqual(callbackListener, callbackTrigger);
+
+                if (cachedEnterTrigger != null)
+                {
+                    Assert.AreNotEqual(cachedEnterTrigger, callbackTrigger);
+                }
+                cachedEnterTrigger = callbackTrigger;
+
+                enterCount++;
+            };
+
+            _proximityChecker.OnStay += (ElementJs callbackListener, ElementJs callbackTrigger) => {
+                Assert.AreEqual(listener, callbackListener);
+                Assert.AreNotEqual(callbackListener, callbackTrigger);
+
+                if (cachedStayTrigger != null)
+                {
+                    Assert.AreNotEqual(cachedStayTrigger, callbackTrigger);
+
+                }
+                cachedStayTrigger = callbackTrigger;
+
+                stayCount++;
+            };
+
+            _proximityChecker.OnExit += (ElementJs callbackListener, ElementJs callbackTrigger) => {
+                Assert.AreEqual(listener, callbackListener);
+                Assert.AreNotEqual(callbackListener, callbackTrigger);
+
+                if (cachedExitTrigger != null)
+                {
+                    Assert.AreNotEqual(cachedExitTrigger, callbackTrigger);
+
+                }
+                cachedExitTrigger = callbackTrigger;
+
+                exitCount++;
+            };
+
+            // Check for no-op
+            _elementA.transform.position = new Vec3(10, 0, 0);
+            _elementB.transform.position = new Vec3(-10, 0, 0);
+            _proximityChecker.Update();
+            CheckCallbackCounts(0, 0, 0);
+
+            // Have one element enter
+            _elementA.transform.position = new Vec3(5, 0, 0);
+            _proximityChecker.Update();
+            CheckCallbackCounts(1, 0, 0);
+            Assert.AreEqual(_elementA, cachedEnterTrigger);
+            cachedEnterTrigger = null;
+
+            // Move the other element in range
+            _elementB.transform.position = new Vec3(-5, 0, 0);
+            _proximityChecker.Update();
+            CheckCallbackCounts(2, 1, 0); // _elementA is still in range, so it should have stayed
+            Assert.AreEqual(_elementB, cachedEnterTrigger);
+            Assert.AreEqual(_elementA, cachedStayTrigger);
+            cachedStayTrigger = null;
+
+            // Have them both stay a frame
+            _proximityChecker.Update();
+            CheckCallbackCounts(2, 3, 0);
+            cachedStayTrigger = null;
+
+            // Have one exit
+            _elementA.transform.position = new Vec3(20, 0, 0);
+            _proximityChecker.Update();
+            CheckCallbackCounts(2, 4, 1);
+            Assert.AreEqual(_elementA, cachedExitTrigger);
+            Assert.AreEqual(_elementB, cachedStayTrigger);
+            cachedExitTrigger = null;
+
+            // Have the other exit
+            _elementB.transform.position = new Vec3(20, 0, 0);
+            _proximityChecker.Update();
+            CheckCallbackCounts(2, 4, 2);
+            Assert.AreEqual(_elementB, cachedExitTrigger);
+        }
+
+
+        // Helpers
+
+        private ElementJs BuildElementJs(bool isListening, bool isTrigger, float innerRadius, float outerRadius)
+        {
+            ElementJs element = new ElementJs(_engine, null, new Element());
+            _proximityChecker.SetElementState(element, isListening, isTrigger);
+            _proximityChecker.SetElementRadii(element, innerRadius, outerRadius);
+            return element;
+        }
+
+        private void CheckCallbackCounts(int enterCount, int stayCount, int exitCount)
+        {
+            Assert.AreEqual(enterCount, this.enterCount);
+            Assert.AreEqual(stayCount, this.stayCount);
+            Assert.AreEqual(exitCount, this.exitCount);
+        }
+    }
+}
---
+++ Assets/Source/Spire/Scripting/Interop/ProximityChecker.cs
@@ -0,0 +1,265 @@
+﻿using CreateAR.Commons.Unity.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace CreateAR.SpirePlayer.Scripting
+{
+    /// <summary>
+    /// Responsible for calculating distances between elements and emitting proximity events as needed.
+    /// Elements are tracked when their state is updated, and proximity events are based on their set radii values.
+    /// 
+    /// Elements have a distinct difference between "listening" and being a "trigger". This is so that multiple listeners
+    ///     can all be in a scene overlapping each other, but not constantly emitting events off of each other. Triggers
+    ///     colliding with listening elements cause proximity events. An element can be listening and/or a trigger.
+    /// </summary>
+    public class ProximityChecker
+    {
+        /// <summary>
+        /// Invoked when a trigger comes in range of a listening element. 
+        /// The first parameter is the listening element, the second is the trigger.
+        /// </summary>
+        public Action<ElementJs, ElementJs> OnEnter;
+
+        /// <summary>
+        /// Invoked every frame when a trigger has entered but not exited a listening element.
+        /// The first parameter is the listening element, the second is the trigger.
+        /// </summary>
+        public Action<ElementJs, ElementJs> OnStay;
+
+        /// <summary>
+        /// Invoked when a trigger exits a listening element.
+        /// The first parameter is the listening element, the second is the trigger.
+        /// </summary>
+        public Action<ElementJs, ElementJs> OnExit;
+
+        /// <summary>
+        /// Helper class used to represent an element and its current configuration for proximity checking
+        /// </summary>
+        private class ElementConfig
+        {
+            /// <summary>
+            /// Element to represent
+            /// </summary>
+            public ElementJs Element;
+
+            /// <summary>
+            /// Whether the Element should react to triggers
+            /// </summary>
+            public bool IsListening;
+
+            /// <summary>
+            /// Whether the Element should trigger proximity events with listeners
+            /// </summary>
+            public bool IsTrigger;
+
+            /// <summary>
+            /// Radius where triggers will cause enter events
+            /// </summary>
+            public float InnerRadius;
+            
+            /// <summary>
+            /// Radius where colliding triggers will cause exit events
+            /// </summary>
+            public float OuterRadius;
+        }
+
+        /// <summary>
+        /// Helper class used to represent ongoing collisions. This is used to determine if events should be enters vs stays.
+        /// Stored ElementConfigs are not directional.
+        /// </summary>
+        private class Collision
+        {
+            /// <summary>
+            /// Configuration for one Element in a collision.
+            /// </summary>
+            public ElementConfig A;
+
+            /// <summary>
+            /// Configuration for the other Element in a collision.
+            /// </summary>
+            public ElementConfig B;
+
+            public Collision(ElementConfig a, ElementConfig b)
+            {
+                A = a;
+                B = b;
+            }
+        }
+
+        /// <summary>
+        /// List of all elements that have been updated as listening or a trigger.
+        /// </summary>
+        private List<ElementConfig> _activeElements = new List<ElementConfig>();
+
+        /// <summary>
+        /// List of all collisions, so we can determine if any collision is an extry or a stay event.
+        /// </summary>
+        private List<Collision> _collisions = new List<Collision>();
+
+
+        /// <summary>
+        /// Updates an Element with regards to its current listening state, and whether it is a trigger or not.
+        /// </summary>
+        /// <param name="element"></param>
+        public void SetElementState(ElementJs element, bool isListening, bool isTrigger)
+        {
+            // Find an existing config, or make a new one
+            ElementConfig config = FindElementConfig(element);
+            if (config == null)
+            {
+                config = new ElementConfig();
+                config.Element = element;
+                _activeElements.Add(config);
+            }
+
+            // Update values
+            config.IsListening = isListening;
+            config.IsTrigger = isTrigger;
+
+            // Remove if needed
+            if (!config.IsListening && !config.IsTrigger)
+            {
+                _activeElements.Remove(config);
+
+                // Remove any active collisions
+                int collisionsLen = _collisions.Count;
+                for (int i = 0; i < collisionsLen; i++)
+                {
+                    Collision collision = _collisions[i];
+                    if (collision.A.Element == element || collision.B.Element == element)
+                    {
+                        InvokeCallbacks(OnExit, collision.A, collision.B);
+                        _collisions.RemoveAt(i--);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Updates an Elements' radii values
+        /// </summary>
+        /// <param name="element">Element to update</param>
+        /// <param name="innerRadius">Radius that'll trigger enter events</param>
+        /// <param name="outerRadius">Raadius that'll trigger exit events</param>
+        public void SetElementRadii(ElementJs element, float innerRadius, float outerRadius)
+        {
+            ElementConfig config = FindElementConfig(element);
+            if (config != null)
+            {
+                config.InnerRadius = Math.Max(0, innerRadius);
+                config.OuterRadius = Math.Max(innerRadius + 0.25f, outerRadius);
+            }
+            else
+            {
+                Log.Warning(this, "Attempting to set radii for untracked element");
+            }
+        }
+
+        /// <summary>
+        /// Checks whether any proximity events should be dispatched or not.
+        /// </summary>
+        public void Update()
+        {
+            int elementCount = _activeElements.Count;
+            for (int i = 0; i < elementCount; i++)
+            {
+                for (int j = i + 1; j < elementCount; j++)
+                {
+                    ElementConfig configA = _activeElements[i];
+                    ElementConfig configB = _activeElements[j];
+
+                    // Early out if the collision wouldn't invoke callbacks
+                    if (!ShouldProcessCollision(configA, configB)) continue;
+
+                    // Check for prior collision
+                    Collision collision = null;
+                    foreach (Collision cachedCollision in _collisions)
+                    {
+                        if ((configA.Element == cachedCollision.A.Element || configA.Element == cachedCollision.B.Element) &&
+                            (configB.Element == cachedCollision.A.Element || configB.Element == cachedCollision.B.Element))
+                        {
+                            collision = cachedCollision;
+                            break;
+                        }
+                    }
+
+                    // Calculate distance
+                    float distance = Vec3.Distance(configA.Element.transform.position, configB.Element.transform.position);
+
+                    // Determine proximity change
+                    if (collision == null)
+                    {
+                        // If we're not already in collision, use inner radii to check for enter
+                        float radiiSum = configA.InnerRadius + configB.InnerRadius;
+
+                        if (distance - radiiSum < 0)
+                        {
+                            InvokeCallbacks(OnEnter, configA, configB);
+                            _collisions.Add(new Collision(configA, configB));
+                        }
+                    }
+                    else
+                    {
+                        // Otherwise, use outer to check for exit
+                        float radiiSum = configA.OuterRadius + configB.OuterRadius;
+
+                        if (distance - radiiSum < 0)
+                        {
+                            InvokeCallbacks(OnStay, configA, configB);
+                        }
+                        else
+                        {
+                            InvokeCallbacks(OnExit, configA, configB);
+                            _collisions.Remove(collision);
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Finds an ElementConfig for a given Element
+        /// </summary>
+        /// <param name="element">Element to get Configuration for.</param>
+        /// <returns>Valid ElementConfig, or null.</returns>
+        private ElementConfig FindElementConfig(ElementJs element)
+        {
+            int elementsLen = _activeElements.Count;
+            for (int i = 0; i < elementsLen; i++)
+            {
+                if (_activeElements[i].Element == element)
+                {
+                    return _activeElements[i];
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Determines if two ElementConfigs are a valid combination capable of invoking proximity events
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        private bool ShouldProcessCollision(ElementConfig a, ElementConfig b)
+        {
+            // A valid collision requires at least 1 listener & trigger between non-nested Elements
+            return ((a.IsListening && b.IsTrigger) || (b.IsListening && a.IsTrigger))
+                && !a.Element.isChildOf(b.Element) && !b.Element.isChildOf(a.Element);
+
+        }
+
+        /// <summary>
+        /// Invokes a given Action for the provided ElementConfigs, depending on their listener/trigger relationship(s).
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        private void InvokeCallbacks(Action<ElementJs, ElementJs> action, ElementConfig a, ElementConfig b)
+        {
+            if (action == null) return;
+            if (a.IsListening && b.IsTrigger) action(a.Element, b.Element);
+            if (b.IsListening && a.IsTrigger) action(b.Element, a.Element);
+        }
+    }
+}
