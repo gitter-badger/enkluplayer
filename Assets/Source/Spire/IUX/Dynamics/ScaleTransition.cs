798e64e2c641e642a0a21119846cd8360b2f1a13
Benjamin Jordan <bmj465@gmail.com>
2018-08-23 15:40:29 -0700

    CodeFactor feedback.

--- Assets/Source/Spire/IUX/Dynamics/ScaleTransition.cs
+++ Assets/Source/Spire/IUX/Dynamics/ScaleTransition.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace CreateAR.SpirePlayer.IUX
+﻿namespace CreateAR.SpirePlayer.IUX
 {
     using System;
     using System.Collections.Generic;
     using UnityEngine;
-
-    namespace CreateAR.SpirePlayer.IUX
-    {
-    }
-
+    
     /// <summary>
     /// Applies a scale transition to elements added + removed.
     /// </summary>
