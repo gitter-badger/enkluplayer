99f415fb37b460626334468b8792c056632eb81e
Benjamin Jordan <bmj465@gmail.com>
2018-08-24 14:48:18 -0700

    Added reload event and better handling of bools.

---
+++ Assets/Source/Messages/ToApplication/Scene/BridgeHelperRefreshElementScriptEvent.cs
@@ -0,0 +1,16 @@
+﻿using LightJson;
+
+namespace CreateAR.SpirePlayer
+{
+    /// <summary>
+    /// Refreshes a script.
+    /// </summary>
+    public class BridgeHelperRefreshElementScriptEvent
+    {
+        /// <summary>
+        /// Id of the element to refresh.
+        /// </summary>
+        [JsonName("id")]
+        public string Id;
+    }
+}
