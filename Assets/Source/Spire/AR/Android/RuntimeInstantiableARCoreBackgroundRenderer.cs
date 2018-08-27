7dff4f4ff8b4164ec78008affd920be7c120f178
brian <cybergen@gmail.com>
2018-08-27 10:33:22 -0700

    Fix for ios build failure. Comments on class

--- Assets/Source/Spire/AR/Android/RuntimeInstantiableARCoreBackgroundRenderer.cs
+++ Assets/Source/Spire/AR/Android/RuntimeInstantiableARCoreBackgroundRenderer.cs
@@ -2,8 +2,18 @@ using GoogleARCore;
 
 namespace CreateAR.SpirePlayer.AR
 {
+    /// <summary>
+    /// A helper class that allows us to add GoogleARCore::ARCoreBackgroundRenderer
+    /// components at runtime without triggering an unnecessary error due to a field
+    /// check in the OnEnable function.
+    /// </summary>
     public class RuntimeInstantiableARCoreBackgroundRenderer : ARCoreBackgroundRenderer
     {
+        /// <summary>
+        /// MonoBehaviour Awake Method. We're disabling the component immediately
+        /// to allow the instantiator to assign fields on ARCoreBackgroundRenderer
+        /// prior to re-enabling.
+        /// </summary>
         private void Awake()
         {
             enabled = false;
