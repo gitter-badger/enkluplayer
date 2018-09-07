00c94f8a859f83d4f553e3e84acd3c49289a2697
James Sundra <pojo226@gmail.com>
2018-09-07 10:34:34 -0700

    Change Proximity to only use horizontal distance for everything, for now

--- Assets/Source/Math/Vec3.cs
+++ Assets/Source/Math/Vec3.cs
@@ -256,5 +256,16 @@ namespace CreateAR.SpirePlayer
         {
             return (from - to).Magnitude;
         }
+
+        public static float DistanceHorizontalSqr(Vec3 from, Vec3 to)
+        {
+            var diff = from - to;
+            return diff.x * diff.x + diff.z * diff.z;
+        }
+
+        public static float DistanceHorizontal(Vec3 from, Vec3 to)
+        {
+            return (float) Math.Sqrt(DistanceHorizontalSqr(from, to));
+        }
     }
 }
