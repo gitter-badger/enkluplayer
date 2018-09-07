a546325222da16673ff8f8ec35aa9f66dd9a9f01
James Sundra <pojo226@gmail.com>
2018-09-07 12:09:03 -0700

    Add PlayerJs to AppJs

--- Assets/Source/Spire/Scripting/Interface/App/AppJsApi.cs
+++ Assets/Source/Spire/Scripting/Interface/App/AppJsApi.cs
@@ -14,14 +14,17 @@
         /// The app's elements.
         /// </summary>
         public readonly AppElementsJsApi elements;
-        
+
+        public readonly PlayerJs player;
+
         /// <summary>
         /// Constructor.
         /// </summary>
-        public AppJsApi(AppScenesJsApi scenes, AppElementsJsApi elements)
+        public AppJsApi(AppScenesJsApi scenes, AppElementsJsApi elements, PlayerJs player)
         {
             this.scenes = scenes;
             this.elements = elements;
+            this.player = player;
         }
     }
 }
