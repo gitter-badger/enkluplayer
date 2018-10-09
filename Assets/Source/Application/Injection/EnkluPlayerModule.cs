304d3aa019d358280f407b232e4f335293360ea1
Benjamin Jordan <bmj465@gmail.com>
2018-10-09 09:12:05 -0700

    Switch back to script cache. This will break script updating when working with a local in-memory instance of Trellis.

--- Assets/Source/Application/Injection/EnkluPlayerModule.cs
+++ Assets/Source/Application/Injection/EnkluPlayerModule.cs
@@ -604,9 +604,7 @@ namespace CreateAR.EnkluPlayer
 #if UNITY_WEBGL
                     binder.Bind<IScriptCache>().To<PassthroughScriptCache>().ToSingleton();
 #else
-                    //binder.Bind<IScriptCache>().To<StandardScriptCache>().ToSingleton();
-
-                    binder.Bind<IScriptCache>().To<PassthroughScriptCache>().ToSingleton();
+                    binder.Bind<IScriptCache>().To<StandardScriptCache>().ToSingleton();
 #endif
                 }
                 
