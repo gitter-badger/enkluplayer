3813948aaa181efd463987ef7aafad56e00bc0dd
thegoldenmule <bmj465@gmail.com>
2018-12-05 09:19:32 -0800

    Simplifying interface.

--- Assets/Source/Player/Scripting/Interface/Drawing/DrawingJsApi.cs
+++ Assets/Source/Player/Scripting/Interface/Drawing/DrawingJsApi.cs
@@ -2,76 +2,153 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using CreateAR.EnkluPlayer.Scripting;
+using Jint;
+using Jint.Native;
 using UnityEngine;
+
+using JsFunc = System.Func<Jint.Native.JsValue, Jint.Native.JsValue[], Jint.Native.JsValue>;
 
 namespace CreateAR.EnkluPlayer
 {
     public class DrawingJsApi
     {
-        private readonly Stack<Matrix4x4> _transformStack = new Stack<Matrix4x4>();
-        private Matrix4x4 _current;
+        private readonly List<JsFunc> _cbs = new List<JsFunc>();
 
-        public DrawingJsApi()
+        private readonly ContextJsApi _context = new ContextJsApi();
+
+        public void render(Engine engine, JsFunc fn)
         {
-            _current = Matrix4x4.identity;
+
+            fn(
+                JsValue.FromObject(engine, this),
+                new[] {JsValue.FromObject(engine, _context)});
+        }
+    }
+
+    public class ContextJsApi
+    {
+        private Color _color;
+
+        public ContextJsApi()
+        {
+            ResetState();
         }
 
-        public void translate(Vec3 to)
+        [DenyJsAccess]
+        public void ResetState()
         {
-            translate(to.x, to.y, to.z);
+            _color = Color.white;
+        }
+        
+        public ContextJsApi color(Col4 col)
+        {
+            color(col.r, col.g, col.b, col.a);
+
+            return this;
         }
 
-        public void translate(float x, float y, float z)
+        public ContextJsApi color(float r, float g, float b, float a)
         {
-            _current = _current * Matrix4x4.Translate(new Vector3(x, y, z));
+            _color = new Color(r, g, b, a);
+
+            return this;
         }
 
-        public void rotateX(float rad)
+        public ContextJsApi alpha(float a)
         {
-            _current = _current * Matrix4x4.Rotate(Quaternion.AngleAxis(
-               Mathf.Rad2Deg * rad,
-               new Vector3(1, 0, 0)));
+            color(_color.r, _color.g, _color.b, a);
+
+            return this;
         }
 
-        public void rotateY(float rad)
+        public ContextJsApi red(float r)
         {
-            _current = _current * Matrix4x4.Rotate(Quaternion.AngleAxis(
-               Mathf.Rad2Deg * rad,
-               new Vector3(0, 1, 0)));
+            color(r, _color.g, _color.b, _color.a);
+
+            return this;
         }
 
-        public void rotateZ(float rad)
+        public ContextJsApi green(float g)
         {
-            _current = _current * Matrix4x4.Rotate(Quaternion.AngleAxis(
-               Mathf.Rad2Deg * rad,
-               new Vector3(0, 0, 1)));
+            color(_color.r, g, _color.b, _color.a);
+
+            return this;
         }
 
-        public void scale(Vec3 to)
+        public ContextJsApi blue(float b)
         {
-            scale(to.x, to.y, to.z);
+            color(_color.r, _color.g, b, _color.a);
+
+            return this;
         }
 
-        public void scale(float x, float y, float z)
+        public ContextJsApi line(Vec3 from, Vec3 to)
         {
-            _current = _current * Matrix4x4.Scale(new Vector3(x, y, z));
+            line(from.x, from.y, from.z, to.x, to.y, to.z);
+
+            return this;
         }
 
-        public void pushMatrix()
+        public ContextJsApi line(
+            float xf, float yf, float zf,
+            float xt, float yt, float zt)
         {
-            _transformStack.Push(_current);
+            GL.Begin(GL.LINES);
+            GL.LoadProjectionMatrix(Camera.main.projectionMatrix);
+            GL.Color(_color);
+            {
+                GL.Vertex(new Vector3(xf, yf, zf));
+                GL.Vertex(new Vector3(xt, yt, zt));
+            }
+            GL.End();
 
-            _current = Matrix4x4.identity;
+            return this;
         }
 
-        public void popMatrix()
+        public ContextJsApi lines(Vec3[] points)
         {
-            _current = _transformStack.Pop();
+            GL.Begin(GL.LINES);
+            GL.LoadProjectionMatrix(Camera.main.projectionMatrix);
+            GL.Color(_color);
+            {
+                for (int i = 0, len = points.Length; i < len; i += 2)
+                {
+                    GL.Vertex(points[i].ToVector());
+                    GL.Vertex(points[i + 1].ToVector());
+                }
+            }
+            GL.End();
+
+            return this;
         }
 
-        public void resetMatrix()
+        public ContextJsApi linestrip(Vec3[] points)
         {
-            _current = Matrix4x4.identity;
+            GL.Begin(GL.LINE_STRIP);
+            GL.LoadProjectionMatrix(Camera.main.projectionMatrix);
+            GL.Color(_color);
+            {
+                for (int i = 0, len = points.Length; i < len; i++)
+                {
+                    GL.Vertex(points[i].ToVector());
+                }
+            }
+            GL.End();
+
+            return this;
+        }
+
+        public ContextJsApi box(float size)
+        {
+            box(size, size, size);
+
+            return this;
+        }
+
+        public ContextJsApi box(float width, float height, float depth)
+        {
+            return this;
         }
     }
 }
