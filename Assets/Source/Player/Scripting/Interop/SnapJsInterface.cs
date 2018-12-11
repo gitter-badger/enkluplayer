a62589f163da4d6b67f096f7c27083f9166132aa
James Sundra <pojo226@gmail.com>
2018-12-10 18:41:28 -0800

    Upgrade SnapJsInterface to include an optional callback for sucess/failure in scripting

--- Assets/Source/Player/Scripting/Interop/SnapJsInterface.cs
+++ Assets/Source/Player/Scripting/Interop/SnapJsInterface.cs
@@ -2,6 +2,8 @@
 using CreateAR.Commons.Unity.Logging;
 using CreateAR.Trellis.Messages;
 using CreateAR.Trellis.Messages.TriggerSnap;
+using Jint.Native;
+using JsFunc = System.Func<Jint.Native.JsValue, Jint.Native.JsValue[], Jint.Native.JsValue>;
 
 namespace CreateAR.EnkluPlayer
 {
@@ -37,6 +39,16 @@ namespace CreateAR.EnkluPlayer
         /// </summary>
         public void trigger(string tag)
         {
+            trigger(tag, null);
+        }
+
+        /// <summary>
+        /// Triggers a snap to be taken. Notifies the callback on success/failure.
+        /// </summary>
+        /// <param name="tag"></param>
+        /// <param name="callback"></param>
+        public void trigger(string tag, JsFunc callback)
+        {
             Log.Info(this, "Trigger a snap.");
 
             _preferences
@@ -61,19 +73,39 @@ namespace CreateAR.EnkluPlayer
                             if (response.Payload.Success)
                             {
                                 Log.Info(this, "Trigger succeeded.");
+                                InvokeCallback(callback, true);
                             }
                             else
                             {
                                 Log.Warning(this,
                                     "Trigger could not be sent : {0}",
                                     response.Payload.Error);
+                                InvokeCallback(callback, false, response.Payload.Error);
                             }
                         })
-                        .OnFailure(exception => Log.Warning(this,
-                            "Trigger could not be fired : {0}",
-                            exception));
+                        .OnFailure(exception =>
+                        {
+                            Log.Warning(this,
+                                "Trigger could not be fired : {0}",
+                                exception);
+                            InvokeCallback(callback, false, exception.ToString());
+                        });
                 })
                 .OnFailure(ex => Log.Error(this, "Could not get user preferences : {0}", ex));
+        }
+
+        /// <summary>
+        /// Invokes a JsFunc with the specified parameters.
+        /// </summary>
+        /// <param name="callback"></param>
+        /// <param name="success"></param>
+        /// <param name="msg"></param>
+        private void InvokeCallback(JsFunc callback, bool success, string msg = "")
+        {
+            if (callback != null)
+            {
+                callback(null, new[] { new JsValue(success), new JsValue(msg) });
+            }
         }
     }
 }
