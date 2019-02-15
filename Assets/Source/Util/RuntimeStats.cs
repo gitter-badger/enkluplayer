8928172ee59cbcefce824c673fcbe38d055283df
James Sundra <pojo226@gmail.com>
2019-02-15 10:02:41 -0800

    Super basic POCO for stats to include in crash reporting

---
+++ Assets/Source/Util/RuntimeStats.cs
@@ -0,0 +1,49 @@
+using CreateAR.EnkluPlayer.IUX;
+using UnityEngine;
+
+namespace CreateAR.EnkluPlayer
+{
+    public class RuntimeStats
+    {
+        public class DeviceInfo
+        {
+            public float Battery;
+
+            public float ReservedMemory;
+            public float AllocatedMemory;
+            public float MonoMemory;
+            public float GpuMemory;
+            public float GraphicsDriverMemory;
+
+            public float AvailableMemory; //
+        }
+
+        public class CameraInfo
+        {
+            public Vector3 Position;
+            public Quaternion Rotation;
+        }
+
+        public class AnchorsInfo
+        {
+            public struct State
+            {
+                public WorldAnchorWidget.WorldAnchorStatus Status;
+                public float TimeUnlocated;
+            }
+            
+        }
+
+        public class ExperienceInfo
+        {
+            public string ExperienceId; //
+            public string AssetState; //
+            public string ScriptState; //
+        }
+        
+        public readonly DeviceInfo Device = new DeviceInfo();
+        public readonly CameraInfo Camera = new CameraInfo();
+        public readonly AnchorsInfo Anchors = new AnchorsInfo();
+        public readonly ExperienceInfo Experience = new ExperienceInfo();
+    }
+}
