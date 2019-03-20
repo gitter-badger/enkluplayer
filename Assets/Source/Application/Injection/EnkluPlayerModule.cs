b379754ff472128062bac0ced6853f82b7eddf34
Matt Bolt <mbolt35@gmail.com>
2019-03-20 14:14:17 -0400

    Do not add a log target if the target is disabled.

--- Assets/Source/Application/Injection/EnkluPlayerModule.cs
+++ Assets/Source/Application/Injection/EnkluPlayerModule.cs
@@ -773,6 +773,11 @@ namespace CreateAR.EnkluPlayer
         /// <returns></returns>
         private static ILogTarget Target(InjectionBinder binder, LogAppConfig.TargetConfig config)
         {
+            if (!config.Enabled)
+            {
+                return null;
+            }
+
             switch (config.Target)
             {
                 case "unity":
