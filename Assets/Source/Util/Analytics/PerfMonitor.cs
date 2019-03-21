a2354553704572a245a30dba0ea59e2129e01205
thegoldenmule <bmj465@gmail.com>
2019-03-21 10:59:54 -0700

    Fix crash issue.

--- Assets/Source/Util/Analytics/PerfMonitor.cs
+++ Assets/Source/Util/Analytics/PerfMonitor.cs
@@ -257,7 +257,7 @@ namespace CreateAR.EnkluPlayer
         {
 #if NETFX_CORE
             Log.Warning(
-                this,
+                Memory,
                 "Memory usage warning: detected increase to '{0}'.",
                 Windows.System.MemoryManager.AppMemoryUsageLevel);
 #endif
